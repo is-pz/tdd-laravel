077f03e550f08a7afdb383b6b97425d2292016b7
Israel Paz <israel_paz@msn.com>
2022-08-04 11:46:05 -0500

    Clase 9 actualizar registro

--- tests/Feature/Http/Controllers/RepositoryControllerTest.php
+++ tests/Feature/Http/Controllers/RepositoryControllerTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 use App\Models\User;
+use App\Models\Repository;
 
 class RepositoryControllerTest extends TestCase
 {
@@ -50,6 +51,47 @@ class RepositoryControllerTest extends TestCase
         ->assertRedirect('repositories');
 
         $this->assertDatabaseHas('repositories', $data);
+    }
+
+//     public function test_update(){
+
+//         $repository = Repository::factory()->create();
+
+//         $data = [
+//                 'url' => $this->faker->url,
+//                 'description' => $this->faker->text,
+//         ];
+
+//         $user = User::factory()->create();
+//         $this->actingAs($user)
+//         ->put("repositories/$repository->id", $data)
+//         ->assertRedirect("repositories/$repository->id/edit");
+
+//         $this->assertDatabaseHas('repositories', $data);
+//     }
+
+
+    public function test_validate_store(){
+
+        $user = User::factory()->create();
+        
+        $this->actingAs($user)
+                ->post('repositories', [])
+                ->assertStatus(302)
+                ->assertSessionHasErrors('url', 'description');
+    }
+
+    public function test_validate_update(){
+
+        $repository = Repository::factory()->create();
+
+        $user = User::factory()->create();
+        
+        $this->actingAs($user)
+                ->put("repositories/$repository->id", [])
+                ->assertStatus(302)
+                ->assertSessionHasErrors('url', 'description');
+
     }
 
 
